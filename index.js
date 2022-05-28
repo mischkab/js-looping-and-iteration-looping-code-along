dcd95a91cacd801be89301d412cc5bf3cdbbe9e1
Mischka Booley <mischka@gearbox.com.au>
2022-05-28 21:04:58 +1000

    complete looping and iterating lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards (names, event) {
+  const array = [];
+  for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  };
+  return array
+};
+
+function countDown(integer) {
+  let count = integer;
+  while (count >= 0) {
+    console.log(count);
+    count--;
+  };
+};
